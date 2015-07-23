24b8dd0d45d355f06ddbd960a1d7f5c87f0d1ef7
ITIM <itim@MobilityMacs-Mac-mini.local>
2015-07-23 16:38:19 +0530

    Initial Commit

---
+++ ABC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABC
+//
+//  Created by ITIM  on 23/07/15.
+//  Copyright (c) 2015 Quinnox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ABC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ABC
+//
+//  Created by ITIM  on 23/07/15.
+//  Copyright (c) 2015 Quinnox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
